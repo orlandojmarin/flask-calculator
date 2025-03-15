5a1b6ec3af4a5b44630c901bc510dec688b35202
Orlando Marin <orlandomarin@orlandos-air.lan>
2025-03-14 21:57:48 -0400

    Added the basic calculator operations and updated the function names

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/add/<int:a>/<int:b>')
-def add(a, b):
+def addition(a, b):
     result = a + b
     return render_template("index.html", operation="Addition", result=result)
 
 @app.route('/subtract/<int:a>/<int:b>')
-def subtract(a, b):
+def subtraction(a, b):
     result = a - b
     return render_template("index.html", operation="Subtraction", result=result)
 
 @app.route('/multiply/<int:a>/<int:b>')
-def multiply(a, b):
+def multiplication(a, b):
     result = a * b
     return render_template("index.html", operation="Multiplication", result=result)
 
 @app.route('/divide/<int:a>/<int:b>')
-def divide(a, b):
+def division(a, b):
     if b == 0:
         return render_template("index.html", operation="Division", result="Error: Cannot divide by zero")
     result = a / b
